1c490d22917aa0a60d0c9d15a294d68bf369f16c
astromasoud <masoud.gp@gmail.com>
2023-03-05 01:35:06 +0300

    Added some comments

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ UpDir=/opt/script/mainservername
 
 BwLimit=2000
 
-# Delete file if exists on destination
+# Delete the file if it exists on destination
 ssh -p $SshPort $SshUser@$ServerAddr "rm -f $UpDir/$UpFileName"
 # Send the file [again]
 rsync -v -e "ssh -p $SshPort" --progress --bwlimit=$BwLimit $ScriptDir/$UpFileName $SshUser@$ServerAddr:$UpDir >> $LogDir/$LogFile
